1b3699e689453f4479b2b63ae5b174c07778ad23
SergioPC <sergiolb@hotmail.es>
2024-05-21 11:12:18 -0500

    Tercera prueba

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-$(document).ready(function (){
-    //Solo permite archivos de imagen
-    $('#exampleInputEmail1').on('dragover', function (event) {
-        event.preventDefault();
-        var items = event.originalEvent.dataTransfer.items;
-        for (var i = 0; i < items.length; i++) {
-            var item = items[i];
-            if (item.kind === 'file') {
-                var type = item.type;
-                if (type !== 'image/png' && type !== 'image/jpeg' && type !== 'image/jpg') {
-                    $('#modalError').modal('show');
-                    return;
-                }
-            }
+$(document).ready(function () {
+    //Muestra la imagen seleccionada
+    $('#file').on('change', function () {
+        var file = this.files[0];
+        var type = file.type;
+        var reader = new FileReader();
+
+        reader.onloadend = function () {
+            $("#imagen").attr("src", reader.result);
+        }
+
+        if (file) {
+            reader.readAsDataURL(file);
+            $("#btnEliminar").prop("disabled", false)
+        } else {
+            $("#imagen").attr("src", "");
+        }
+
+        if (type !== 'image/png' && type !== 'image/jpeg' && type !== 'image/jpg') {
+            $('#modalError').modal('show')
+            $("#file").val("")
+            return
         }
     });
 
-    //Muestra la imagen seleccionada
-    $('#exampleInputEmail1').on('change', function() {
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            $('.figure-img').attr('src', e.target.result);
-        }
-        reader.readAsDataURL(this.files[0]);
-    });
-    
+    $("#btnEliminar").on("click", function () {
+        $("#imagen").attr("src", "")
+        $("#file").val("")
+        $("#btnEliminar").prop("disabled", true)
+    })
+
 })
-    
+
 
