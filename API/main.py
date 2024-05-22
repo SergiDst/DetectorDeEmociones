8120bffd2c77e8bbc9fc49e6b8da1d82be5435e6
SergioPC <sergiolb@hotmail.es>
2024-05-22 12:57:34 -0500

    Sexta prueba
    Solo falta la logica (Ya recibe el archivo)

--- API/main.py
+++ API/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form, Depends
+from starlette.responses import HTMLResponse
 import tensorflow as tf
 import tensorflow_hub as hub
 from PIL import Image
@@ -7,12 +8,14 @@ from io import BytesIO
 import cv2
 import numpy as np
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from dataclasses import dataclass
 
 with tf.keras.utils.custom_object_scope({'KerasLayer': hub.KerasLayer}):
-    model = tf.keras.models.load_model('./API/modelo/modelo_entrenado.h5')
+    model = tf.keras.models.load_model('modelo/modelo_entrenado.h5')
 
-def categorizar(url):
-  """ url = data.get("url") """
+""" def categorizar(url):
+  url = data.get("url")
   respuesta = requests.get(url)
   img = Image.open(BytesIO(respuesta.content))
   img = np.array(img).astype(float)/255
@@ -22,25 +25,35 @@ def categorizar(url):
 
 url = 'https://www.tophoy.com/wp-content/uploads/2019/06/mujer-llorando.jpg'
 prediccion = categorizar (url)
-print(prediccion)
+print(prediccion) """
 
 
-""" app = FastAPI()
+app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+origins = [
+   "http://127.0.0.1:5500",
+   "http://localhost:5500"
+]  
 
-class ImageData(BaseModel):
-    url: str 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@dataclass
+class SimpleModel:
+    filetest: UploadFile = Form(...)
 
 @app.post("/resultado")
-async def categorizar(data: ImageData):
-  url = data.get("url")
-  respuesta = requests.get(url)
-  img = Image.open(BytesIO(respuesta.content))
+async def categorizar(form_data: SimpleModel = Depends()):
+  """ respuesta = requests.get(url)
+  img = Image.open(BytesIO(file))
   img = np.array(img).astype(float)/255
   img = cv2.resize(img, (224,224))
   prediccion = model.predict(img.reshape(-1, 224, 224, 3))
-  return np.argmax(prediccion[0], axis=-1), prediccion  """
+  return np.argmax(prediccion[0], axis=-1), prediccion """
+  return { 'filename': form_data.filetest.file}
  
--- script.js
+++ script.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
         } else if (type !== 'image/png' && type !== 'image/jpeg' && type !== 'image/jpg') {
             $('#modalError').modal('show')
             $("#file").val("")
-            $("#imagen").attr("src", "");
+            $("#imagen").attr("src", "")
             return
         }
     });
@@ -24,24 +24,28 @@ $(document).ready(function () {
     $("#btnEliminar").on("click", function () {
         $("#imagen").attr("src", "")
         $("#file").val("")
+        $("#resultado").attr("src", "./assets/images/neutral.png")
+        $("#txtResultado").html("<p>Neutral</p>")
         $("#btnEliminar").prop("disabled", true)
     })
 
     $("#btnEnviar").on("click", function () {
-        var imagenUrl = $("#imagen").attr("src");
-            $.ajax({
-            url: "miapi",
+        var file = $("#file")[0].files[0]
+        const formData = new FormData();
+        formData.append("filetest", file);
+        $.ajax({
+            url: "http://127.0.0.1:8082/resultado",
             type: "POST",
-            data: imagenUrl,
-            processData: false,
-            contentType: false,
+            data: formData,
+            processData: false,  // tell jQuery not to process the data
+            contentType: false,  // tell jQuery not to set contentType
             success: function (response) {
                 console.log(response)
-                /* $("#resultado").attr("src", "")
-                $("#btnEnviar").prop("disabled", true) */
             }
         })
-
+        $("#resultado").attr("src", "./assets/images/feliz.png")
+        $("#txtResultado").html("<p>Estas feliz</p>")
+        $("#btnEnviar").prop("disabled", true)
     })
 
 })
